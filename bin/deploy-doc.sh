0507f8998e0df414fe88d565e7f072c2f5a3b93a
Takashi Kitajima <inc@2inc.org>
2016-02-29 03:02:39 +0900

    fix deploy bug

--- bin/deploy-doc.sh
+++ bin/deploy-doc.sh
@@ -13,5 +13,5 @@ if [[ "master" != "$TRAVIS_BRANCH" ]]; then
 fi
 
 git add -A
-git commit -m "[ci skip] release branch update from travis $TRAVIS_COMMIT"
-git push --quiet "https://${GH_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git" release 2> /dev/null
+git commit -m "[ci skip] gh-pages branch update from travis $TRAVIS_COMMIT"
+git push --quiet "https://${GH_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git" gh-pages 2> /dev/null
