0c70858938df8c9a58c527ae022ecc5b58b28b22
Takashi Kitajima <inc@2inc.org>
2016-05-24 10:42:20 +0900

    Changed indent style

--- bin/deploy-doc.sh
+++ bin/deploy-doc.sh
@@ -3,13 +3,13 @@
 set -e
 
 if [[ "false" != "$TRAVIS_PULL_REQUEST" ]]; then
-	echo "Not deploying pull requests."
-	exit
+  echo "Not deploying pull requests."
+  exit
 fi
 
 if [[ "master" != "$TRAVIS_BRANCH" ]]; then
-	echo "Not on the 'master' branch."
-	exit
+  echo "Not on the 'master' branch."
+  exit
 fi
 
 cd gh-pages
