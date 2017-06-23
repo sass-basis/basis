22a196d207d5f33a3db50672cd8069af8b354991
Takashi Kitajima <inc@2inc.org>
2017-06-23 23:19:42 +0900

    Some fixes

--- bin/gh-pages.sh
+++ bin/gh-pages.sh
@@ -7,10 +7,10 @@ if [[ "false" != "$TRAVIS_PULL_REQUEST" ]]; then
   exit
 fi
 
-#if [[ "master" != "$TRAVIS_BRANCH" ]]; then
-#  echo "Not on the 'master' branch."
-#  exit
-#fi
+if [[ "master" != "$TRAVIS_BRANCH" ]]; then
+  echo "Not on the 'master' branch."
+  exit
+fi
 
 git clone -b gh-pages --quiet https://github.com/sass-basis/basis.git gh-pages
 cd gh-pages
