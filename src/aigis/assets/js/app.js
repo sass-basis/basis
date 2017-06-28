b6178d1b230d2d4055550e9cf0c7ab8588784b5b
Takashi Kitajima <inc@2inc.org>
2017-06-28 16:15:55 +0900

    Bug fix styleguide

--- src/aigis/assets/js/app.js
+++ src/aigis/assets/js/app.js
@@ -1,32 +1,34 @@
 'use strict';
 
-import BasisHamburgerBtn from '../../../js/hamburger-btn.js';
-new BasisHamburgerBtn({
+import '../../../js/basis';
+
+import SgBasisHamburgerBtn from '../../../js/hamburger-btn.js';
+new SgBasisHamburgerBtn({
   btn: '.sg-c-hamburger-btn'
 });
 
-import BasisDrawer from '../../../js/drawer.js';
-new BasisDrawer({
+import SgBasisDrawer from '../../../js/drawer.js';
+new SgBasisDrawer({
   drawer : '.sg-c-drawer',
   toggle : '.sg-c-drawer__toggle',
   submenu: '.sg-c-drawer__submenu'
 });
 
-import BasisNavbar from '../../../js/navbar.js';
-new BasisNavbar({
+import SgBasisNavbar from '../../../js/navbar.js';
+new SgBasisNavbar({
   item   : '.sg-c-navbar__item',
   submenu: '.sg-c-navbar__submenu',
   subitem: '.sg-c-navbar__subitem'
 });
 
-import BasisPageEffect from '../../../js/page-effect.js';
-new BasisPageEffect({
+import SgBasisPageEffect from '../../../js/page-effect.js';
+new SgBasisPageEffect({
   pageEffect: '.sg-c-page-effect',
   duration: 200
 });
 
-import BasisSelect from '../../../js/select.js';
-new BasisSelect({
+import SgBasisSelect from '../../../js/select.js';
+new SgBasisSelect({
   select: '.sg-c-select',
   label : '.sg-c-select__label'
 });
