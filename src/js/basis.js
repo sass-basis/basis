abe01de9b8b30fd2c8e6517a1c553ace5779aa48
Takashi Kitajima <inc@2inc.org>
2020-03-23 15:12:13 +0900

    Fix #122

--- src/js/basis.js
+++ src/js/basis.js
@@ -5,9 +5,6 @@ import BasisDrawer from './_drawer.js';
 import BasisHamburgerBtn from './_hamburger-btn.js';
 import BasisNavbar from './_navbar.js';
 import BasisPageEffect from './_page-effect.js';
-import BasisSelect from './_select.js';
-import BasisCheckbox from './_checkbox.js';
-import BasisRadio from './_radio.js';
 
 document.addEventListener(
   'DOMContentLoaded',
@@ -18,9 +15,6 @@ document.addEventListener(
     new BasisHamburgerBtn();
     new BasisNavbar();
     new BasisPageEffect();
-    new BasisSelect();
-    new BasisCheckbox();
-    new BasisRadio();
   },
   false
 );
